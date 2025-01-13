1eb4fb1876630b536790e50d8c498ec182c38274
mingthang <mthangdev@gmail.com>
2025-01-13 10:14:55 +0700

    Update: setup project 01/0.6

--- 01 - Getting Started/06 - Coordinate Systems/4.6.fragment_shader.glsl
+++ 01 - Getting Started/06 - Coordinate Systems/4.6.fragment_shader.glsl
@@ -1,6 +1,14 @@
 #version 460 core
+
+in vec3 ourColor;
+in vec2 TexCoord;
+
 out vec4 FragColor;
 
-void main() {
-    FragColor = vec4(1.0, 1.0, 1.0, 1.0);
+uniform sampler2D texture1;
+uniform sampler2D texture2;
+
+void main() 
+{
+    FragColor = mix(texture(texture1, TexCoord), texture(texture2, TexCoord), 0.2);
 }
---
+++ 01 - Getting Started/06 - Coordinate Systems/Shader.h
@@ -0,0 +1,92 @@
+#pragma once
+
+#include <glad/glad.h>
+#include <GLFW/glfw3.h>
+
+#include <iostream>
+#include <fstream>
+#include <sstream>
+
+class Shader
+{
+public:
+	unsigned int ID;
+
+	Shader(const char* vertexPath, const char* fragmentPath)
+	{
+		std::string vertexCode, fragmentCode;
+		std::ifstream vShaderFile, fShaderFile;
+		vShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		fShaderFile.exceptions(std::ifstream::failbit | std::ifstream::badbit);
+		try
+		{
+			vShaderFile.open(vertexPath);
+			fShaderFile.open(fragmentPath);
+			std::stringstream vShaderStream, fShaderStream;
+			vShaderStream << vShaderFile.rdbuf();
+			fShaderStream << fShaderFile.rdbuf();
+			vShaderFile.close();
+			fShaderFile.close();
+			vertexCode = vShaderStream.str();
+			fragmentCode = fShaderStream.str();
+		}
+		catch (std::ifstream::failure& e)
+		{
+			std::cout << "ERROR::SHADER::FILE_NOT_SUCCESSFULLY_READ: " << e.what() << std::endl;
+		}
+
+		const char* vShaderCode = vertexCode.c_str();
+		const char* fShaderCode = fragmentCode.c_str();
+		unsigned int vertexID, fragmentID;
+		// vertex shader source attaching, check compile
+		vertexID = glCreateShader(GL_VERTEX_SHADER);
+		glShaderSource(vertexID, 1, &vShaderCode, NULL);
+		glCompileShader(vertexID);
+		checkCompileErrors(vertexID, "VERTEX");
+		// fragment shader source attaching, check compile
+		fragmentID = glCreateShader(GL_FRAGMENT_SHADER);
+		glShaderSource(fragmentID, 1, &fShaderCode, NULL);
+		glCompileShader(fragmentID);
+		checkCompileErrors(fragmentID, "FRAGMENT");
+		// create shader program
+		ID = glCreateProgram();
+		glAttachShader(ID, vertexID);
+		glAttachShader(ID, fragmentID);
+		glLinkProgram(ID);
+		checkCompileErrors(ID, "PROGRAM");
+
+		glDeleteShader(vertexID);
+		glDeleteShader(fragmentID);
+	}
+
+	void use() const
+	{
+		glUseProgram(ID);
+	}
+
+private:
+	void checkCompileErrors(GLuint id, std::string type)
+	{
+		GLint success;
+		GLchar infoLog[1024];
+		if (type != "PROGRAM")
+		{
+			glGetShaderiv(id, GL_COMPILE_STATUS, &success);
+			if (!success)
+			{
+				glGetShaderInfoLog(id, 1024, NULL, infoLog);
+				std::cout << "ERROR::SHADER_COMPILATION_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+		}
+		else
+		{
+			glGetProgramiv(id, GL_LINK_STATUS, &success);
+			if (!success)
+			{
+				glGetProgramInfoLog(id, 1024, NULL, infoLog);
+				std::cout << "ERROR::PROGRAM_LINKING_ERROR of type: " << type << "\n" << infoLog << "\n -- --------------------------------------------------- -- " << std::endl;
+			}
+		}
+	}
+
+};
