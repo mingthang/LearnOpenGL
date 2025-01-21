a81c655ea6d9986b20f247e5927f28120b43b21f
mingthang <mthangdev@gmail.com>
2025-01-21 19:46:58 +0700

    Update: Back-face culling

--- Lab/Matric Stack/Source.cpp
+++ Lab/Matric Stack/Source.cpp
@@ -62,6 +62,8 @@ int main()
 
 	glEnable(GL_DEPTH_TEST);
 	glDepthFunc(GL_LEQUAL);
+	glEnable(GL_CULL_FACE);
+	glCullFace(GL_BACK);
 
 	while (!glfwWindowShouldClose(window))
 	{
@@ -131,6 +133,7 @@ void setupVertices()
 
 void display()
 {
+	
 	float currentTime = (float)glfwGetTime();
 
 	glClearColor(0.0, 0.0, 0.0, 1.0);
